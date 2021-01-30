ec02c58a7d41b55b15dd773e82fe1fe3eb3cbffc
qiuz <zhu_anly@163.com>
2021-01-30 22:03:19 +0800

    fix: loading zoom

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,15 +5,13 @@
  * @Last Modified by: qiuz
  */
 
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout, Spin } from 'antd';
 import './index.less';
-
-import { registerMicroApps, start, initGlobalState, runAfterFirstMounted } from 'qiankun';
+import { registerMicroApps, start, initGlobalState } from 'qiankun';
 import { HeaderBar } from 'components';
-
 import { MENU_LIST } from './mock';
 import { useViewport } from 'hooks';
 
@@ -29,50 +27,42 @@ Spin.setDefaultIndicator(
   </div>
 );
 
-export default class Dashboard extends Component<any, any> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      loading: false,
-      subAppList: []
-    };
-  }
+export default function Dashboard(props: any) {
+  const [loading, setLoading] = useState(false);
+  const { width } = useViewport();
 
-  componentDidMount() {
-    const { width } = useViewport();
-    const loader = (loading: any) => {
-      const container = document.getElementById('subapp-container');
-      ReactDOM.render(
-        <>
-          <Spin
-            style={{
-              position: 'fixed',
-              top: '50%',
-              left: 'calc(50% - 100px)',
-              zIndex: 99,
-              zoom: width <= 600 ? 0.3 : 1
-            }}
-            spinning={loading}
-          />
-          <div id="subapp-viewport" />
-        </>,
-        container
-      );
+  const loader = () => {
+    const container = document.getElementById('subapp-container');
+    ReactDOM.render(
+      <>
+        <Spin
+          style={{
+            position: 'fixed',
+            top: '50%',
+            left: 'calc(50% - 100px)',
+            zIndex: 99,
+            zoom: width <= 600 ? 0.3 : 1
+          }}
+          spinning={loading}
+        />
+        <div id="subapp-viewport" />
+      </>,
+      container
+    );
+  };
+
+  const subAppList: any = MENU_LIST.map((menu: any) => {
+    return {
+      name: menu.title,
+      entry: `//${window.location.host}${menu.entry}`,
+      container: '#subapp-viewport',
+      loader,
+      activeRule: menu.url
     };
-    const subAppList = MENU_LIST.map((menu: any) => {
-      return {
-        name: menu.title,
-        entry: `//${window.location.host}${menu.entry}`,
-        container: '#subapp-viewport',
-        loader,
-        activeRule: menu.url
-      };
-    });
-    this.setState({
-      loading: true,
-      subLoader: loader,
-      subAppList
-    });
+  });
+
+  useEffect(() => {
+    setLoading(true);
 
     registerMicroApps(subAppList);
 
@@ -84,24 +74,22 @@ export default class Dashboard extends Component<any, any> {
      * Step4 启动应用
      */
     start({ sandbox: { experimentalStyleIsolation: true } });
-  }
+  });
 
-  render() {
-    return (
-      <BrowserRouter>
-        <Layout className="widgets-layout">
-          <Header className="header">
-            <div className="logo">
-              <a href="/">Qiuz</a>
-            </div>
-            <HeaderBar />
-          </Header>
-          <Content className="site-layout-background">
-            <div id="subapp-container" className="view"></div>
-          </Content>
-          <Footer className="widgets-layout-container-footer">Widgets ©2021 Created by qiuz</Footer>
-        </Layout>
-      </BrowserRouter>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Layout className="widgets-layout">
+        <Header className="header">
+          <div className="logo">
+            <a href="/">Qiuz</a>
+          </div>
+          <HeaderBar />
+        </Header>
+        <Content className="site-layout-background">
+          <div id="subapp-container" className="view"></div>
+        </Content>
+        <Footer className="widgets-layout-container-footer">Widgets ©2021 Created by qiuz</Footer>
+      </Layout>
+    </BrowserRouter>
+  );
 }
